a81f864a481ed099d72aa06ce9ef8b367dfbf73a
JV-VM <joaovictoroliveiradourado2007@gmail.com>
2024-10-28 01:01:21 -0300

    update: AM Light Theme is made

---
+++ lib/ready_themes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData amLightThemeStyleData = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color(0xAF81B89A),
+    onPrimary: Color(0xFFFFEA19),
+    onSecondary: Color(0),
+  ),
+);
